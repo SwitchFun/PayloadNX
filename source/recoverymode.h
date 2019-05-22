3b1edcd4515098b4110a79d9a964f4df52d4b4c8
SwitchFun <48801768+SwitchFun@users.noreply.github.com>
2019-05-22 16:31:50 -0500

    Create recoverymode.h

---
+++ source/recoverymode.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <switch.h>
+
+#define BCT_SZ 0x4000
+#define BCT_KEY_OFF 0x210
+#define KB_SZ 0xB0
+#define NUM_KBS 32
+
+#define RCM_XOR 0x77
